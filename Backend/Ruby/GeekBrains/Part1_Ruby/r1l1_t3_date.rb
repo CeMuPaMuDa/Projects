7461c7bc0cc81872799eaa0dec0fdb9a66ccdfe9
CeMuPaMuDa <bepyc9i@gmail.com>
2022-05-11 14:07:36 +0300

    add task3

---
+++ Backend/Ruby/GeekBrains/Part1_Ruby/r1l1_t3_date.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
+
+
+Bundler.require(:default)
+
+I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
+I18n.default_locale = :ru
+
+puts "Текущая дата: #{I18n.l(Time.now, :format => :my)}"
