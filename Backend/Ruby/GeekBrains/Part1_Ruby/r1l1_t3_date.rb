cc4b6f12320e81461f253550fb5395df046f56b0
CeMuPaMuDa <bepyc9i@gmail.com>
2022-05-11 17:47:17 +0300

    fix first lesson

--- Backend/Ruby/GeekBrains/Part1_Ruby/r1l1_t3_date.rb
+++ Backend/Ruby/GeekBrains/Part1_Ruby/r1l1_t3_date.rb
@@ -7,4 +7,4 @@ Bundler.require(:default)
 I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
 I18n.default_locale = :ru
 
-puts "Текущая дата: #{I18n.l(Time.now, :format => :my)}"
+puts "Текущая дата: #{I18n.l(Time.now, format: :my)}"
