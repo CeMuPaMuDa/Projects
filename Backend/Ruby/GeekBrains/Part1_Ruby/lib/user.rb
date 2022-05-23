e43beb2b09229faa867ffc530b2e52fe0199fcd7
CeMuPaMuDa <bepyc9i@gmail.com>
2022-05-23 17:40:18 +0300

    add task3

---
+++ Backend/Ruby/GeekBrains/Part1_Ruby/lib/user.rb
@@ -0,0 +1,9 @@
+class User
+  def full_name
+    'Владимир Князевич'
+  end
+
+  def profession
+    'князь Киевський'
+  end
+end
---
+++ Backend/Ruby/GeekBrains/Part1_Ruby/r1l2_t3_user.rb
@@ -0,0 +1,7 @@
+require_relative 'lib/user'
+
+user = User.new
+
+puts 'Информация о пользователе:'
+puts "Фамилия и имя пользователя: #{user.full_name}"
+puts "Профессия: #{user.profession}"
